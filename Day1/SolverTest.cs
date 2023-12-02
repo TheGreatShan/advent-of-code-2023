ee92b1b1cec26fdf1fa718b19390c6ae9e2820fe
Shansai Muraleetharan <shansai@muraleetharan.ch>
2023-12-02 07:17:29 +0100

    solver returns first and last number

---
+++ Day1/SolverTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Day1;
+
+public class SolverTest
+{
+    [Fact]
+    void should_return_first_and_last_number()
+    {
+        var actual = Solver.GetKey("dfhdjfh134dhfsj7h");
+        Assert.Equal("17", actual);
+    }
+
+    [Fact]
+    public void should_return_sum()
+    {
+        var keys = new List<string>{"45", "34"};
+
+        var actual = Solver.GetSum(keys);
+
+        Assert.Equal(79, actual);
+    }
+}
